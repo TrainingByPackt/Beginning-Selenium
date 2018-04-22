39529ea0a29cbe3edad50eb0eec8d2a78b346771
Diego Molina <diemol@gmail.com>
2018-04-22 21:00:34 +0200

    Adding Activity01Lesson02

---
+++ docs/lesson_2/src/main/java/com/beginningselenium/selenium/Activity01Lesson02.java
@@ -0,0 +1,17 @@
+package com.beginningselenium.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Activity01Lesson02 {
+
+    private void activityLesson02AutomationScript(){
+
+        WebDriver driver = new ChromeDriver();
+
+        // the other script code will go here
+
+        driver.quit();
+    }
+
+}
