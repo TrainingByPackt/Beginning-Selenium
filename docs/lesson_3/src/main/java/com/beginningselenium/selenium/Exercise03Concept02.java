695351f07d7dd92e72523034632f6f6947994b6b
Diego Molina <diemol@gmail.com>
2018-05-06 18:02:25 +0200

    Adding code for exercise in concept 2

--- docs/lesson_3/src/main/java/com/beginningselenium/selenium/Exercise03Concept02.java
+++ docs/lesson_3/src/main/java/com/beginningselenium/selenium/Exercise03Concept02.java
@@ -1,12 +1,16 @@
 package com.beginningselenium.selenium;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
-public class Exercise03Concept01 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Exercise03Concept02 {
 
   public static void main(String[] args) {
     interactingWithTextAreas();
@@ -19,35 +23,50 @@ public class Exercise03Concept01 {
     try {
       driver.get("https://trainingbypackt.github.io/Beginning-Selenium/lesson_3/exercise_3_1.html");
 
-      WebElement textArea = driver.findElement(By.id("aboutYourself"));
+      Select singleChoiceList = new Select(driver.findElement(By.id("monthOfBirth")));
 
-      if (textArea.isEnabled() && textArea.isDisplayed()) {
-        System.out.println("The text area is visible and displayed");
-
-        // Checking for an empty text area
-        if ("".equals(textArea.getAttribute("value"))) {
-          System.out.println("The text area is empty");
+      if (!singleChoiceList.isMultiple() && singleChoiceList.getOptions().size() == 13) {
+        System.out.println("The list does not accept multiple choices and contains 13 options (including 'Choose...').");
+        singleChoiceList.selectByVisibleText("February");
+        if (singleChoiceList.getFirstSelectedOption().getText().equalsIgnoreCase("February")) {
+          System.out.println("It worked, February was selected as the month.");
         } else {
-          System.out.println("Something is wrong, the text area NOT empty");
+          System.out.println("Something went wrong, February is not selected.");
         }
-
-        textArea.sendKeys("This is a sample text.");
-
-        if ("This is a sample text.".equalsIgnoreCase(textArea.getAttribute("value"))) {
-          System.out.println("Text was correctly typed into the text area.");
-        } else {
-          System.out.println("Something went wrong, text was not typed into the text area.");
-        }
-
-        textArea.clear();
-        if ("".equals(textArea.getAttribute("value"))) {
-          System.out.println("The text area is empty after cleaning it though a Selenium command");
-        } else {
-          System.out.println("Something went wrong, the text area was not cleaned");
-        }
-
       } else {
-        System.out.println("Something went wrong, the text area is not visible and displayed");
+        System.out.println("Something went wrong, the selection list is not configured as expected");
+      }
+
+      Select multipleChoiceList = new Select(driver.findElement(By.id("hobbies")));
+
+      if (multipleChoiceList.isMultiple() && multipleChoiceList.getOptions().size() == 4) {
+        System.out.println("The list does accept multiple choices and contains 4 options.");
+        multipleChoiceList.selectByVisibleText("Reading");
+        multipleChoiceList.selectByVisibleText("Sports");
+        multipleChoiceList.selectByVisibleText("Traveling");
+
+        multipleChoiceList.deselectByVisibleText("Sports");
+
+        if (multipleChoiceList.getAllSelectedOptions().size() == 2) {
+          System.out.println("It worked, 2 options have been chosen");
+        } else {
+          System.out.println("Something went wrong, a different number from 2 options have been chosen");
+        }
+
+        List<String> expectedSelection = Arrays.asList("Reading", "Traveling");
+        List<String> actualSelection = new ArrayList<String>();
+
+        for (WebElement element : multipleChoiceList.getAllSelectedOptions()) {
+          actualSelection.add(element.getText());
+        }
+
+        if (actualSelection.containsAll(expectedSelection)) {
+          System.out.println("It worked, all expected elements were selected.");
+        } else {
+          System.out.println("Something went wrong, not all the expected elements were selected.");
+        }
+      } else {
+        System.out.println("Something went wrong, the multiple selection list is not configured as expected");
       }
 
     } finally {
@@ -56,39 +75,3 @@ public class Exercise03Concept01 {
   }
 
 }
-
-/*
-public class ActivityLesson03 {
-
-  public void Test() {
-    WebElement element = driver.findElement(By.ID("firstName"));
-    if (element.isEnabled() && element.isDisplayed())
-    {
-      System.out.println("Load of the test worked, the textbox is enabled and visible");
-      if (element.getText() == "")
-      {
-          element.sendKeys("Mike");
-          if (element.getText() == "Mike")
-           System.out.println("Load of the test worked, the textbox is empty. 'Mike' is the new name");
-          else
-            System.out.println("Something went wrong with the test, 'Mike' was not set as a name'");
-      }
-      else
-      {
-          element.clear();
-          element.sendKeys("Jake");
-          if (element.getText() == "Jake")
-          System.out.println("Load of the test worked, the textbox is empty. 'Jake' is the new name");
-         else
-           System.out.println("Something went wrong with the test, 'Jake' was not set as a name'");
-      }
-    }
-    else
-      System.out.println("Something went wrong with the test, the textbox is not enabled or it is invisible");
-
-  }
-
-  driver.quit();
-
-}
- */
