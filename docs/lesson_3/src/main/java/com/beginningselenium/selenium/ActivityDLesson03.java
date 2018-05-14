f7d24bf38ab68f88585bb714e9a34c9eed2ccce1
Diego Molina <diemol@gmail.com>
2018-05-15 00:20:46 +0200

    Adding proposed solution for activity D, lesson 3

--- docs/lesson_3/src/main/java/com/beginningselenium/selenium/ActivityDLesson03.java
+++ docs/lesson_3/src/main/java/com/beginningselenium/selenium/ActivityDLesson03.java
@@ -6,47 +6,77 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class ActivityCLesson03 {
+import java.util.List;
+
+public class ActivityDLesson03 {
 
   public static void main(String[] args) {
-    interactingWithTextAreas();
+    locatingElements();
   }
 
-  private static void interactingWithTextAreas() {
+  private static void locatingElements() {
 
     WebDriver driver = new ChromeDriver();
 
     try {
-      driver.get("https://trainingbypackt.github.io/Beginning-Selenium/lesson_3/exercise_3_1.html");
+      driver.get("https://trainingbypackt.github.io/Beginning-Selenium/lesson_3/activity_3_D-1.html");
 
-      WebElement firstName = driver.findElement(By.id("firstName"));
-      firstName.sendKeys("John");
+      // Using id
       WebElement lastName = driver.findElement(By.id("lastName"));
-      lastName.sendKeys("Doe");
+      if (lastName.isDisplayed()) {
+        System.out.println("Automation script worked, lastName element is visible");
+      } else {
+        System.out.println("Something went wrong with the test, lastName element is not visible");
+      }
 
-      Select dayOfBirth = new Select(driver.findElement(By.id("dayOfBirth")));
-      dayOfBirth.selectByVisibleText("20");
-      Select monthOfBirth = new Select(driver.findElement(By.id("monthOfBirth")));
-      monthOfBirth.selectByVisibleText("March");
-      WebElement yearOfBirth = driver.findElement(By.id("yearOfBirth"));
-      yearOfBirth.sendKeys("1990");
+      // Using name
+      WebElement hobbies = driver.findElement(By.name("hobbies"));
+      if (hobbies.isDisplayed()) {
+        System.out.println("Automation script worked, hobbies element is visible");
+      } else {
+        System.out.println("Something went wrong with the test, hobbies element is not visible");
+      }
 
-      Select hobbies = new Select(driver.findElement(By.id("hobbies")));
-      hobbies.selectByVisibleText("Reading");
-      hobbies.selectByVisibleText("Sports");
+      // Using class name (in this case, coincidentally the first name is the first element of this kind
+      WebElement firstName = driver.findElement(By.className("form-control"));
+      if (firstName.isDisplayed()) {
+        System.out.println("Automation script worked, hobbies element is visible");
+      } else {
+        System.out.println("Something went wrong with the test, hobbies element is not visible");
+      }
 
-      WebElement masters = driver.findElement(By.cssSelector("input[value='masters']"));
-      masters.click();
+      // Using HTML tag
+      List<WebElement> div = driver.findElements(By.tagName("div"));
+      if (div.size() > 0) {
+        System.out.println("Automation script worked, there are more than zero divs on the page");
+      } else {
+        System.out.println("Something went wrong with the test, divs could not be located");
+      }
 
-      WebElement emailUpdates = driver.findElement(By.id("emailUpdates"));
-      emailUpdates.click();
+      // Using link
+      WebElement spanishLink = driver.findElement(By.linkText("Spanish"));
+      String link = spanishLink.getAttribute("href");
+      if (!"".equals(link)) {
+        System.out.println("Automation script worked, the link was found");
+      } else {
+        System.out.println("Something went wrong with the test, link was not found");
+      }
 
-      WebElement aboutYourself = driver.findElement(By.id("aboutYourself"));
-      aboutYourself.sendKeys("I think Selenium is getting easier and easier!");
+      // Using xpath (try to avoid it as much as possible, CSS is a better option)
+      Select dayOfBirth = new Select(driver.findElement(By.xpath("//select")));
+      if (dayOfBirth.getOptions().size() > 0) {
+        System.out.println("Automation script worked, the element was found");
+      } else {
+        System.out.println("Something went wrong with the test, the element was not found");
+      }
 
-      WebElement submit = driver.findElement(By.id("submit"));
-      submit.click();
-
+      // Using CSS
+      WebElement firstNameWithCss = driver.findElement(By.cssSelector("#firstName"));
+      if (firstNameWithCss.isDisplayed()) {
+        System.out.println("Automation script worked, firstName with CSS element is visible");
+      } else {
+        System.out.println("Something went wrong with the test, firstName with CSS element is not visible");
+      }
 
     } finally {
       driver.quit();
