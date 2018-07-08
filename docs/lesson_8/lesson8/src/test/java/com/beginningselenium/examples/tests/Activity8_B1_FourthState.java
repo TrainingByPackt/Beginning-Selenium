fd803b652ed1032fcc4c3e0561db9d63dbd64d22
Diego Molina <diemol@gmail.com>
2018-07-08 22:15:32 +0200

    Different states of the activity solution.

--- docs/lesson_8/lesson8/src/test/java/com/beginningselenium/examples/tests/Activity8_B1_FourthState.java
+++ docs/lesson_8/lesson8/src/test/java/com/beginningselenium/examples/tests/Activity8_B1_FourthState.java
@@ -4,13 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-
-public class Activity8_B1_FirstState {
+public class Activity8_B1_FourthState {
 
   private WebDriver driver;
 
@@ -27,43 +27,33 @@ public class Activity8_B1_FirstState {
   }
 
   @Test
-  public void TimingTest() throws InterruptedException {
+  public void TimingTest() {
     driver.get("https://trainingbypackt.github.io/Beginning-Selenium/lesson_8/activity_8-B-1.html");
 
-    // Sleeping 12 seconds because the button does not seem to take longer than that
+    WebDriverWait wait = new WebDriverWait(driver, 12);
+
     System.out.println("Waiting for the first button");
-    Thread.sleep(12 * 1000);
-    WebElement firstButton = driver.findElement(By.id("firstButton"));
+    WebElement firstButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("firstButton")));
     System.out.println("Clicking on the first button");
     firstButton.click();
 
-    // Sleeping again until the next button shows up
     System.out.println("Waiting for the second button");
-    Thread.sleep(12 * 1000);
-    WebElement secondButton = driver.findElement(By.id("secondButton"));
+    WebElement secondButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("secondButton")));
     System.out.println("Clicking on the second button");
     secondButton.click();
 
-    // Sleeping again until the next button shows up
     System.out.println("Waiting for the third button");
-    Thread.sleep(12 * 1000);
-    WebElement thirdButton = driver.findElement(By.id("thirdButton"));
+    WebElement thirdButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("thirdButton")));
     System.out.println("Clicking on the third button");
     thirdButton.click();
 
-    // Sleeping again until the next button shows up
-    Thread.sleep(12 * 1000);
     System.out.println("Waiting for the fourth button");
-    WebElement fourthButton = driver.findElement(By.id("fourthButton"));
+    WebElement fourthButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("fourthButton")));
     System.out.println("Clicking on the fourth button");
     fourthButton.click();
 
-    // Sleeping again until the next button shows up
     System.out.println("Waiting for the final status");
-    Thread.sleep(5 * 1000);
-    WebElement instruction = driver.findElement(By.id("instruction"));
-    Assert.assertEquals(instruction.getText(), "Activity completed!");
-
+    wait.until(ExpectedConditions.textToBe(By.id("instruction"), "Activity completed!"));
   }
 
 }
